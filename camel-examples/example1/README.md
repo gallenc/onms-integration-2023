e8c9c881192afd4e635a79d5da2d4ad0b5368906
gallenc <cg02r@zepler.net>
2023-04-01 16:49:01 +0100

    added camel rest example - not working

---
+++ camel-examples/example1/README.md
@@ -0,0 +1,6 @@
+# example 1
+
+this taken from https://aruva.medium.com/auto-generate-rest-integrations-from-swagger-in-camel-d75486e030f4
+
+
+mvn archetype:generate  "-DarchetypeGroupId=org.apache.camel.archetypes" "-DarchetypeArtifactId=camel-archetype-spring-boot"  "-DarchetypeVersion=3.11.0"
---
+++ camel-examples/example1/example1/src/main/java/org/opennms/integration/camel/example1/RestRoutes.java
@@ -0,0 +1,27 @@
+package org.opennms.integration.camel.example1;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.LoggingLevel;
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RestRoutes extends RouteBuilder {
+	@Override
+	public void configure() throws Exception {
+		
+		System.out.println("inside routebuilder"+RestRoutes.class);
+		
+	    from( "direct:getUsers")
+	    .log(LoggingLevel.INFO, ">> In here ... Getting all Users")
+	    .transform()
+	    .simple("Hello ${in.body.name}")
+	    .setHeader(Exchange.HTTP_RESPONSE_CODE, constant(200));
+	    
+	    from( "direct:createUser")
+	    .log(LoggingLevel.INFO, ">> In here ... Creating a User")
+	    .transform()
+	    .simple("Hello creating a user ${in.body.name}")
+	    .setHeader(Exchange.HTTP_RESPONSE_CODE, constant(200));
+	 }
+}

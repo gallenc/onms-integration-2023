30cc2e86ca1993f3c7271e26618248f7dce4de09
gallenc <cgallen@opennms.org>
2023-04-24 20:27:28 +0100

    running pax exam example

---
+++ paxexamtests/paxexamtest-remote/src/test/java/org/opennms/example/paxexam/paxexamtest/MyTest.java
@@ -0,0 +1,67 @@
+package org.opennms.example.paxexam.paxexamtest;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.junit.Assert.assertThat;
+import static org.ops4j.pax.exam.CoreOptions.junitBundles;
+import static org.ops4j.pax.exam.CoreOptions.mavenBundle;
+import static org.ops4j.pax.exam.CoreOptions.options;
+import static org.ops4j.pax.exam.CoreOptions.systemProperty;
+import static org.ops4j.pax.exam.CoreOptions.bootClasspathLibrary;
+import javax.inject.Inject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.ops4j.pax.exam.Configuration;
+import org.ops4j.pax.exam.Option;
+import org.ops4j.pax.exam.junit.PaxExam;
+import org.ops4j.pax.exam.spi.reactors.ExamReactorStrategy;
+import org.ops4j.pax.exam.spi.reactors.PerClass;
+import org.ops4j.pax.exam.util.PathUtils;
+import org.osgi.framework.BundleContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@RunWith(PaxExam.class)
+@ExamReactorStrategy(PerClass.class)
+public class MyTest {
+	private static Logger LOG = LoggerFactory.getLogger(MyTest.class);
+
+	@Inject
+	private BundleContext bc;
+
+	@Configuration
+	public Option[] config() {
+		return options(systemProperty("log4j.configurationFile").value("file:src/test/resources/log4j2.xml"),
+
+				mavenBundle("org.slf4j", "slf4j-api", "1.7.4"),
+				mavenBundle().artifactId("pax-logging-log4j2").groupId("org.ops4j.pax.logging").version("2.0.14"),
+				mavenBundle().artifactId("pax-logging-api").groupId("org.ops4j.pax.logging").version("2.0.14"),
+//            mavenBundle("org.apache.logging.log4j", "log4j-api", "2.11.1"),
+//            mavenBundle("org.apache.logging.log4j", "log4j-core", "2.11.1"),
+//            mavenBundle("org.apache.logging.log4j", "log4j-slf4j-impl", "2.11.1"),
+				junitBundles(),
+
+		// to run forked needs to be on classpath
+		// not needed if native as already on classpath
+           bootClasspathLibrary("mvn:org.slf4j/slf4j-api/1.7.4"),
+           
+           bootClasspathLibrary("mvn:org.ops4j.pax.logging/pax-logging-log4j2/2.0.14"),
+           bootClasspathLibrary("mvn:org.ops4j.pax.logging/pax-logging-api/2.0.14")
+           
+        //   bootClasspathLibrary("mvn:org.apache.logging.log4j/log4j-api/2.11.1"),
+        //   bootClasspathLibrary("mvn:org.apache.logging.log4j/log4j-core/2.11.1"),
+        //   bootClasspathLibrary("mvn:org.apache.logging.log4j/log4j-slf4j-impl/2.11.1")
+
+		);
+	}
+
+	@Test
+	public void shouldHaveBundleContext() {
+		LOG.error("**** make sure we can log error");
+		LOG.info("**** make sure we can log info ");
+		LOG.debug("**** make sure we can log debug");
+		assertThat(bc, is(notNullValue()));
+	}
+
+}
